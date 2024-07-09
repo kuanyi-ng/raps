b5bfe3fa47091c99ca2ff2a96e6fe65974849c3e
Bouknight <szl@ornl.gov>
2024-07-09 12:48:39 -0400

    Updates flops.py and system.json

--- raps/flops.py
+++ raps/flops.py
@@ -1,38 +1,28 @@
-# Cinea nearly 32 theoretical peak petaFLOPS of computing power,
-# https://newsroom.ibm.com/2020-07-07-CINECA-Chooses-IBM-POWER9-with-NVIDIA-GPUs-and-InfiniBand-Network-for-Marconi100-Accelerated-Cluster
 import numpy as np
 from .utils import linear_to_3d_index
+from .config import initialize_config, load_config_variables
 
-#from .config import initialize_config, load_config_variables
-#initialize_config(args.system)
-#load_config_variables(['CPUS_PER_NODE', 'GPUS_PER_NODE'], globals())
+load_config_variables(['CPUS_PER_NODE', 
+                       'GPUS_PER_NODE',
+                       'CPU_PEAK_FLOPS', 
+                       'GPU_PEAK_FLOPS', 
+                       'CPU_FP_RATIO',
+                       'GPU_FP_RATIO',
+                       'TOTAL_NODES',
+                       'DOWN_NODES',
+                       'SC_SHAPE'
+                      ], globals())
 
-# Frontier
-PRECISION = 64
-CPU_CORES = 64
-FLOPS_PER_GPU = 52
-FLOPS_PER_CPU_CORE_PER_CYCLE = 16
-# See https://docs.lumi-supercomputer.eu/hardware/lumig/
-# The cores of this CPU are "Zen 3" compute cores supporting AVX2 256-bit vector instructions 
-# for a maximum throughput of 16 double precision FLOP/clock (AVX2 FMA operations). 
-GHZ = 2E9
+node_peak_flops = CPUS_PER_NODE*CPU_PEAK_FLOPS + GPUS_PER_NODE*GPU_PEAK_FLOPS
+print(f"Node peak FLOPS: {node_peak_flops:.2e}")
 
-cpu_peak_flops = CPU_CORES * FLOPS_PER_CPU_CORE_PER_CYCLE * GHZ
-print(cpu_peak_flops)
-
-gpu_peak_flops = 52E12
-
-node_peak_flops = cpu_peak_flops + 4 * gpu_peak_flops
-
-print(f"{node_peak_flops:.2e}")
-
-system_peak_flops = 74 * 128 * node_peak_flops
-
-print(f"{system_peak_flops:.2e}")
+num_nodes = TOTAL_NODES - len(DOWN_NODES)
+system_peak_flops = num_nodes * node_peak_flops
+print(f"System peak FLOPS: {system_peak_flops:.2e}")
 
 
 def compute_node_flops(cpu_util, gpu_util):
-    return cpu_util * cpu_peak_flops + gpu_util * gpu_peak_flops
+    return CPU_FP_RATIO * cpu_util * CPU_PEAK_FLOPS + GPU_FP_RATIO * gpu_util * GPU_PEAK_FLOPS
 
 
 class FLOPSManager():
@@ -49,5 +39,5 @@ class FLOPSManager():
         return np.sum(self.flop_state)
 
 if __name__ == "__main__":
-    fm = FLOPManager((25, 3, 128))
+    fm = FLOPManager(SC_SHAPE)
     print(fm.flop_state.shape)
