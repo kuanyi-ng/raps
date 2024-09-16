a61c62265e74557373920290e73fde44a13f46e5
Wes Brewer <brewerwh@ornl.gov>
2024-09-16 17:00:15 -0400

    Convert power to utilization arrays - still not working correctly

--- raps/dataloaders/lassen.py
+++ raps/dataloaders/lassen.py
@@ -1,34 +1,26 @@
 import math
+import numpy as np
 import os
 import pandas as pd
 from ..config import load_config_variables
 from ..job import job_dict
+from ..utils import power_to_utilization
 from tqdm import tqdm
 import time
 
-load_config_variables(['TRACE_QUANTA'], globals())
-
-def load_data_with_progress(path):
-    print(f"reading {path}...")
-    chunksize = 10000  # Adjust the chunk size as needed
-    total_rows = sum(1 for _ in open(path)) - 1  # Total rows minus header
-    
-    chunk_list = []  # Store chunks
-    for chunk in tqdm(pd.read_csv(path, chunksize=chunksize), total=total_rows//chunksize):
-        chunk_list.append(chunk)
-    
-    # Concatenate all chunks into a single DataFrame
-    df = pd.concat(chunk_list)
-    return df
+load_config_variables(['TRACE_QUANTA', 
+                       'CPUS_PER_NODE', 
+                       'GPUS_PER_NODE', 
+                       'POWER_GPU_IDLE',
+                       'POWER_GPU_MAX',
+                       'POWER_CPU_IDLE',
+                       'POWER_CPU_MAX'], globals())
 
 def load_data(path, **kwargs):
     """
     Loads data from the given file paths and returns job info.
     """
     nrows = 1E4
-    #alloc_df = load_data_with_progress(os.path.join(path[0], 'final_csm_allocation_history_hashed.csv'))
-    #node_df = load_data_with_progress(os.path.join(path[0], 'final_csm_allocation_node_history.csv'))
-    #step_df = load_data_with_progress(os.path.join(path[0], 'final_csm_step_history.csv'))
     alloc_df = pd.read_csv(os.path.join(path[0], 'final_csm_allocation_history_hashed.csv'), nrows=nrows)
     node_df = pd.read_csv(os.path.join(path[0], 'final_csm_allocation_node_history.csv'), nrows=nrows)
     step_df = pd.read_csv(os.path.join(path[0], 'final_csm_step_history.csv'), nrows=nrows)
@@ -47,6 +39,7 @@ def load_data_from_df(allocation_df, node_df, step_df, **kwargs):
     for _, row in tqdm(allocation_df.iterrows(), total=len(allocation_df), desc="Processing Jobs"):
         start_row = time.time()  # Start timing each row
         node_data = node_df[node_df['allocation_id'] == row['allocation_id']]
+        nodes_required = row['num_nodes']
 
         wall_time_start = time.time()  # Timing wall_time calculation
         wall_time = compute_wall_time(row['begin_time'], row['end_time'])
@@ -55,22 +48,37 @@ def load_data_from_df(allocation_df, node_df, step_df, **kwargs):
 
         # Compute GPU power
         gpu_start = time.time()  # Timing GPU power calculation
-        gpu_energy = node_data['gpu_energy'].sum()
-        gpu_usage = node_data['gpu_usage'].sum()
-        gpu_power = gpu_energy / (gpu_usage / 1E6) if gpu_usage > 0 else 0
-        gpu_trace = [gpu_power] * samples
-        #print(f"GPU power calculation took: {time.time() - gpu_start} seconds")
+        gpu_energy = node_data['gpu_energy'].sum() # Joules
+        gpu_usage = node_data['gpu_usage'].sum()   # microseconds
+        gpu_power = gpu_energy / (gpu_usage / 1E6) / nodes_required if gpu_usage > 0 else 0
+        if gpu_power == 0:
+            continue
+        else:
+            gpu_power_array = np.array([gpu_power] * samples)
+
+        gpu_min_power = nodes_required * POWER_GPU_IDLE * GPUS_PER_NODE
+        gpu_max_power = nodes_required * POWER_GPU_MAX * GPUS_PER_NODE
+        print(gpu_power_array)
+        print(gpu_min_power, gpu_max_power)
+        gpu_util = power_to_utilization(gpu_power_array, gpu_min_power, gpu_max_power)
+        gpu_trace = gpu_util * GPUS_PER_NODE
 
         # Compute CPU power (assuming total energy minus gpu_energy is cpu_energy)
         cpu_start = time.time()  # Timing CPU power calculation
-        total_energy = node_data['energy'].sum()
-        cpu_usage = node_data['cpu_usage'].sum()
+        total_energy = node_data['energy'].sum() # Joules
+        cpu_usage = node_data['cpu_usage'].sum() # nanoseconds
         cpu_energy = total_energy - gpu_energy
-        cpu_power = cpu_energy / (cpu_usage / 1E9) if cpu_usage > 0 else 0
-        cpu_trace = [cpu_power] * samples
+        cpu_power = cpu_energy / (cpu_usage / 1E9) / nodes_required if cpu_usage > 0 else 0
+        cpu_power_array = np.array([cpu_power] * samples)
+
+        cpu_min_power = nodes_required * POWER_CPU_IDLE * CPUS_PER_NODE
+        cpu_max_power = nodes_required * POWER_CPU_MAX * CPUS_PER_NODE
+        cpu_util = power_to_utilization(cpu_power_array, cpu_min_power, cpu_max_power)
+        cpu_trace = cpu_util * CPUS_PER_NODE
+
         #print(f"CPU power calculation took: {time.time() - cpu_start} seconds")
 
-        job_info = job_dict(row['num_nodes'], \
+        job_info = job_dict(nodes_required, \
                             row['primary_job_id'], \
                             cpu_trace, gpu_trace, wall_time, \
                             row['exit_status'], \
